47a8f2d02f4d8ea23c5921969ba290cc978723fc
Ruthik27 <ruthikkale27@gmail.com>
2020-10-14 13:08:13 +0530

    Create Tasks.py
    creating the search button

---
+++ Tasks.py
@@ -0,0 +1,17 @@
+import pymsql
+from tkinter import *
+from tkinter import messagebox
+
+def search():
+    try:
+        con = pymsql.connect(user='root', password='', host='localhost', database='db')
+        cur = con.cursor()
+        sql = "select * from student where Rollno='%s'"%Rollno.get()
+        cur.execute(sql)
+        result = cur.fetchone()
+        name.set(result[2])
+        e1.configure(state = 'disabled')
+        con.close()
+    except:
+        messagebox.showinfo('No Data','No such data available')
+        clear()
