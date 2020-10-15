29008ece03b92303f7acc6092215536c0757d4cc
Ruthik27 <ruthikkale27@gmail.com>
2020-10-15 19:26:21 +0530

    Update Tasks.py
    adding remaing functions

--- Tasks.py
+++ Tasks.py
@@ -16,3 +16,46 @@ def search():
     except:
         messagebox.showinfo('No Data','No such data available')
         clear()
+
+def clear():
+    Rollno.set('')
+    name.set('')
+    age.set('')
+    e1.configure(state='normal')
+
+def add():
+    try:
+        con = pymsql.connect(user='root', password='', host='localhost', database='db')
+        cur = con.cursor()
+        sql = "insert into student values('%s','%s','%s')" %(Rollno.get(),name.get(),age.get())
+        cur.execute(sql)
+        con.commit()
+        con.close()
+        messagebox.showinfo('Success','Rrcord saved...')
+    except:
+        messagebox.showinfo('Error','Error in data entry...')
+
+def update():
+    try:
+        con = pymsql.connect(user='root', password='', host='localhost', database='db')
+        cur = con.cursor()
+        sql = "update student set name = '%s', age = '%s' where Rollno = '%s'" %(name.get(), age.get(), Rollno.get())
+        cur.execute(sql)
+        con.commit()
+        con.close()
+        messagebox.showinfo('Success','Rrcord updated...')
+    except:
+        messagebox.showinfo('Error','Error occured...')
+
+def delete():
+    try:
+        con = pymsql.connect(user='root', password='', host='localhost', database='db')
+        cur = con.cursor()
+        sql = "delete from student where Rollno = '%s'" %(Rollno.get())
+        cur.execute(sql)
+        con.commit()
+        con.close()
+        messagebox.showinfo('Success','Rrcord deleted...')
+    except:
+        messagebox.showinfo('Error','Error occured...')
+        
